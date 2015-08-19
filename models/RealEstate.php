f14e97653cbf556138071eee7ed6e9e3a7208b9b
Fabio Maggio <fabiomaggio@gmail.com>
2015-08-19 15:46:51 +0200

    Create real estate model

---
+++ models/RealEstate.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace infoweb\skarabee\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\db\Query;
+
+class RealEstate extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'real_estates';
+    }
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_at',
+                ],
+                'value' => function() { return time(); },
+            ],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [];
+    }
+}
